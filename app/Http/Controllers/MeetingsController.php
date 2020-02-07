37573757ea11c8695e8dcfaabd0b28ba1087efc2
ericdiedrich <ericdiedrich@users.noreply.github.com>
2020-02-07 10:47:51 +0000

    initialize github

---
+++ app/Http/Controllers/MeetingsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\meetings;
+use Illuminate\Http\Request;
+
+class MeetingsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\meetings  $meetings
+     * @return \Illuminate\Http\Response
+     */
+    public function show(meetings $meetings)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\meetings  $meetings
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(meetings $meetings)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\meetings  $meetings
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, meetings $meetings)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\meetings  $meetings
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(meetings $meetings)
+    {
+        //
+    }
+}
